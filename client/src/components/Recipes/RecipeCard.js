4e543ef3e7e8fe2c271ef6c6d39aef6a86a87d37
Andrea Sekito <andrea@Chonkers-Computer.local>
2020-09-30 16:28:06 -0700

    material-ui integrated into project. popup for recipes on click of recipe card

--- client/src/components/Recipes/RecipeCard.js
+++ client/src/components/Recipes/RecipeCard.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./RecipeCard.css";
+import { Modal } from "@material-ui/core/";
 
 const RecipeCard = ({ recipe }) => {
+  const [open, setOpen] = useState(false);
+
+  const WholeRecipe = (
+    <div className="recipe-popup">
+      <h2>{recipe.name}</h2>
+      <div className="recipe-info">
+        <div className="recipe-name">{recipe.name}</div>
+        <div className="ingredients-list-title">
+          <b>Ingredients:</b>
+        </div>
+        <div className="ingredients-list-container">
+          {recipe.ingredients.map((ingredientElement, idx) => (
+            <div className="individual-ingredient">
+              <div className="ingredient-name">
+                {ingredientElement.ingredient}
+              </div>
+              {ingredientElement.estimated_amount ? (
+                <div>{ingredientElement.estimated_amount}</div>
+              ) : null}
+              {ingredientElement.measured_amount ? (
+                <div>
+                  {ingredientElement.measured_amount.amount}{" "}
+                  {ingredientElement.measured_amount.unit}
+                </div>
+              ) : null}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="recipe-image">Image here</div>
+    </div>
+  );
+
+  const toggle = () => {
+    setOpen(!open);
+  };
+
   return (
-    <div className="recipe-card">
+    <div className="recipe-card" onClick={toggle}>
       <div className="recipe-info">
         <div className="recipe-name">{recipe.name}</div>
         <div className="ingredients-list-title">
@@ -15,18 +53,17 @@ const RecipeCard = ({ recipe }) => {
               <div className="ingredient-name">
                 {ingredientElement.ingredient}
               </div>
-              {/* {ingredientElement.estimated_amount ? (
-              <div>{ingredientElement.estimated_amount}</div>
-            ) : null}
-
-            {ingredientElement.measured_amount ? (
-              <div>
-                {ingredientElement.measured_amount.amount}{" "}
-                {ingredientElement.measured_amount.unit}
-              </div>
-            ) : null} */}
             </div>
           ))}
+          <Modal
+            open={open}
+            onClose={toggle}
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description"
+            className="modal"
+          >
+            {WholeRecipe}
+          </Modal>
         </div>
       </div>
       <div className="recipe-image">Image here</div>
