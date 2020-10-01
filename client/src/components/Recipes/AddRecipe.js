a5bf4755b6e93f1ee317ed25036930ecb5ae00f6
Andrea Sekito <andrea@Chonkers-Computer.local>
2020-09-30 19:39:46 -0700

    basic algorithm to gather ingredient input info

--- client/src/components/Recipes/AddRecipe.js
+++ client/src/components/Recipes/AddRecipe.js
@@ -1,48 +1,68 @@
 import React, { useState } from "react";
 import AddingIngredient from "./AddingIngredient";
 import "./AddRecipe.css";
+import regeneratorRuntime from "regenerator-runtime";
 
 const AddRecipe = () => {
-  const [recipe, setRecipe] = useState({});
+  const [recipeName, setRecipeName] = useState("");
+  const [ingredients, setIngredients] = useState([]);
+
   const addIngredient = (e) => {
     e.preventDefault();
     let input = `
-                <label>
-                  Ingredient:
-                  <input type='text' />
-                </label>
-                <label>
-                  Measurement:
-                  <input type='text' />
-                </label>
-                <label>
-                  Estimated:
-                  <input type='text' />
-                </label>
+                  <form class="new-ingredient-measurements">
+                    <input type="text" name="ingredient" />
+                    <input type="text" name="estimated-amount" />
+                    <input type="number" name="measured-amount" />
+                    <select name="measured-unit">
+                      <option defaultValue="" value="">
+                        -
+                      </option>
+                      <option value="ounce">ounce</option>
+                      <option value="pounds">pound(s)</option>
+                      <option value="pint">pint</option>
+                      <option value="cup">cup</option>
+                      <option value="tablespoons">tablespoons</option>
+                      <option value="teaspoon">teaspoon</option>
+                    </select>
+                </form>
                 `;
     document.getElementById("add-ingred-wrapper").innerHTML += input;
   };
 
+  const createIngredients = async (e) => {
+    e.preventDefault();
+    let hi = document.getElementsByClassName("new-ingredient-measurements");
+    for (let element of hi) {
+      let ingredientObject = {};
+      for (let input of element) {
+        if (input.value) {
+          ingredientObject[input.name] = input.value;
+        }
+      }
+      await setIngredients((ingredients) => [...ingredients, ingredientObject]);
+    }
+  };
+
   return (
     <div className="popup add-recipe">
-      <h1>Add New Recipe</h1>
-      <form className="new-ingredient-form">
+      <h1 onClick={() => console.log(ingredients)}>Add New Recipe</h1>
+      <div className="add-recipe-name">
         <label>
           Recipe Name
-          <input type="text" />
+          <input type="text" name="recipe" />
         </label>
-        <label>
-          Ingredient
-          <input type="text" />
-        </label>
-        <label>
-          Estimated amount:
-          <input type="text" />
-        </label>
-        <label>
-          Measured amount:
-          <input type="number" />
-          <select name="unit">
+      </div>
+      <button onClick={(e) => addIngredient(e)}>+ Ingredient</button>
+      <div id="add-ingred-wrapper">
+        <form className="new-ingredient-measurements">
+          <input type="text" name="ingredient" />
+          <input type="text" name="estimated-amount" />
+          <input type="number" name="measured-amount" />
+          <select name="measured-unit">
+            <option defaultValue="" value="">
+              -
+            </option>
             <option value="ounce">ounce</option>
             <option value="pounds">pound(s)</option>
             <option value="pint">pint</option>
@@ -50,12 +70,35 @@ const AddRecipe = () => {
             <option value="tablespoons">tablespoons</option>
             <option value="teaspoon">teaspoon</option>
           </select>
-        </label>
-        <button onClick={(e) => addIngredient(e)}>+ Ingredient</button>
-        <div id="add-ingred-wrapper"></div>
-      </form>
+        </form>
+      </div>
+
+      <button onClick={(e) => createIngredients(e)}>Add All Ingredients</button>
+      <button>Add Recipe</button>
+      <div>
+        <h2>Preview</h2>
+        {ingredients.map((ingredient, idx) => (
+          <div>{ingredient.ingredient}</div>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default AddRecipe;
+
+// {
+/* <label>
+<input type="text" />
+<input type="number" />
+<select name="unit">
+  <option value="ounce">ounce</option>
+  <option value="pounds">pound(s)</option>
+  <option value="pint">pint</option>
+  <option value="cup">cup</option>
+  <option value="tablespoons">tablespoons</option>
+  <option value="teaspoon">teaspoon</option>
+</select>
+
+}
+*/
