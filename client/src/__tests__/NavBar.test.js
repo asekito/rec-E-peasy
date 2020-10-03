ee36484b93920170c077e9f7872baf8a94ae9d2e
Andrea Sekito <andrea@Chonkers-Computer.local>
2020-10-03 00:30:19 -0700

    set up recipes component test

--- client/src/__tests__/NavBar.test.js
+++ client/src/__tests__/NavBar.test.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
-import { MemoryRouter } from "react-router-dom";
+import { shallow } from "enzyme";
 
 import Nav from "../components/NavBar/Nav";
-import Home from "../components/HomePage/Home";
 
 describe("Nav", () => {
   test("shoulde render navigation bar", () => {
