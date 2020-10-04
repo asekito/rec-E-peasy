825066535dd0b19e61c6cc905f0e49efd18a0c70
Andrea Sekito <andrea@Chonkers-Computer.local>
2020-10-04 07:08:36 -0700

    removal unnecessary imports

--- client/src/components/NavBar/Nav.js
+++ client/src/components/NavBar/Nav.js
@@ -1,24 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Nav.css";
 
 const Nav = () => {
   return (
-    <div id="nav-component">
-      <div id="nav-title">
-        <Link to="/">
-          <h1>Rec-E-Peasy</h1>
+    <div id='nav-component'>
+      <div id='nav-title'>
+        <Link to='/'>
+          <h1 style={{ textDecoration: "none" }}>Rec-E-Peasy</h1>
         </Link>
       </div>
-      <ul id="nav-bar">
-        <li className="nav-tab">
-          <Link to="/">Home</Link>
+      <ul id='nav-bar'>
+        <li className='nav-tab'>
+          <Link to='/'>Home</Link>
         </li>
-        <li className="nav-tab">
-          <Link to="/your-recipes">Your Recipes</Link>
+        <li className='nav-tab'>
+          <Link to='/your-recipes'>Your Recipes</Link>
         </li>
-        <li className="nav-tab">
-          <Link to="/cooking-log">Cooking Log</Link>
+        <li className='nav-tab'>
+          <Link to='/cooking-log'>Cooking Log</Link>
         </li>
       </ul>
     </div>
