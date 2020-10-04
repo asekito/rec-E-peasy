a456d0d0ec2cc73c30be987e3946d27d9ee272ce
Andrea Sekito <andrea@Chonkers-Computer.local>
2020-10-03 22:53:19 -0700

    home page stylings

--- client/src/components/HomePage/Home.js
+++ client/src/components/HomePage/Home.js
@@ -5,24 +5,20 @@ const Home = () => {
   return (
     <div id='homepage-content'>
       <div id='home-bkimg' />
-      <h2
-        style={{
-          color: "white",
-          position: "relative",
-          textAlignLast: "center",
-          paddingTop: 70,
-        }}
-      >
-        Simple and to the point
-      </h2>
+      <h2 className='home title'>simple and to the point . . . :) </h2>
       <div id='home-intro'>
-        Tired of restrictive recipe websites? Tired of having to input every
-        single step of a meal you made from winging it? With Rec-E-Peasy, you
-        can add your recipe its ingredients and you can be as specific with the
-        measurements or not as you want. List them out and boom, you have all
-        the meals ingredients in one space with rough or precise estimates.
-        Simple, to the point, and works with the home chef that loves winging it
-        when it comes to cooking.
+        <p>
+          Tired of restrictive recipe websites? Tired of having to input every
+          single step of a meal you made from winging it?
+        </p>
+        With Rec-E-Peasy, you can add your recipe its ingredients and you can be
+        as specific with the measurements or not as you want. List them out and
+        boom, you have all the meals ingredients in one space with rough or
+        precise estimates.
+        <p>
+          Simple, to the point, and works with the home chef that loves winging
+          it when it comes to cooking.
+        </p>
       </div>
     </div>
   );
