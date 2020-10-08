cd1edfdadfac35167e983fc18c036b6db5f33ed2
asekito <email@email.com>
2020-10-08 09:58:23 -0700

    set up for CookLog fetching and display

--- src/components/CookLog/CookLog.js
+++ src/components/CookLog/CookLog.js
@@ -1,9 +1,21 @@
 import React from "react";
+import "./CookLog.css";
 
 const CookLog = () => {
+  const [cookLogs, setCookLogs] = useState([]);
+
+  // get request for cook logs
+  // setCookLogs
+  // display
+
   return (
-    <div>
+    <div id='cooklog-contents'>
       <h1>Cooking Log</h1>
+      <div id='cooklog-container'>
+        {/* {cookLogs.map((cookLog, idx) => (
+        <div key={idx}>{cookLog.name}</div>
+      ))} */}
+      </div>
     </div>
   );
 };
